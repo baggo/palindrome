954c96f820d46c695448c5cf5e24b4a78b5bc3ea
Dustin Dowell <dustindowell22@gmail.com>
2017-03-17 13:42:51 -0500

    reverse iterate

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function(word){
-  for (var m = word.length, i = ~~(m-- / 2); i--;)
+  for (var m = word.length, i = ~~(m-- / 2); --i;)
     if (word[i] !== word[m - i]) return false
   return true
 }
