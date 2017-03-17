55b7e0f0e0a0994830b4647a5b1f3544a32953ba
Dustin Dowell <dustindowell22@gmail.com>
2017-03-17 13:36:22 -0500

    tests

--- test.js
+++ test.js
@@ -4,8 +4,9 @@ var palin = require('./')
 test('checks palindromes', t => {
   t.plan(3)
 
-  t.true(palin('kek'), 'palindrome 1')
-  t.true(palin('racecar'), 'palindrome 2')
+  t.true(palin('lolol'), 'palindrome 1')
+  t.true(palin('tacocat'), 'palindrome 2')
   t.false(palin('meme'), 'not palindrome')
+  t.false(palin('plump'), 'not palindrome')
 
 })
