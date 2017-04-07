b35d3e8d8da515393824cfa1fa7eaacc07b13998
Dustin Dowell <dustindowell22@gmail.com>
2017-04-07 10:49:24 -0500

    standard js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-module.exports = function(word){
-  for (var m = word.length, i = ~~(m-- / 2); --i;)
-    if (word[i] !== word[m - i]) return false
+module.exports = function (word) {
+  for (var m = word.length, i = ~~(m-- / 2); --i;) {
+    if (word[i] !== word[m - i]) {
+      return false
+    }
+  }
   return true
 }
